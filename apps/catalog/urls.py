92f18954ff765a5581362a7c8997abdd89042e66
NguyenHoaiPhong <nguyenhoaiphong14723@gmail.com>
2026-03-27 14:28:09 +0700

    [update] - add api for get all products

---
+++ apps/catalog/urls.py
@@ -0,0 +1,13 @@
+from rest_framework import routers
+from django.urls import path, include
+
+from .views import ProductViewSet
+
+app_name = "api_catalog"
+
+router = routers.DefaultRouter()
+router.register(r"products", ProductViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import AllowAny
+
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+class ProductPagination(PageNumberPagination):
+    page_size = 24
+    page_size_query_param = "page_size"
+    max_page_size = 100
+    
+
+# Tại sao sử dụng ReadOnlyModelViewSet thay vì ModelViewSet
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Product.objects.select_related("category").all().order_by("-created_at")
+    serializer_class = ProductSerializer
+    permission_classes = [AllowAny]
+    pagination_class = ProductPagination
+    ordering_fields = ["price", "created_at", "rating", "name"]
